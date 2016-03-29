00b158d9b5019624c94756e4c83353947f745d03
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-29 09:29:23 -0400

    Fix JSON parse issue

--- test.rb
+++ test.rb
@@ -14,7 +14,7 @@ class ApplicationTest < Minitest::Test
 
   def test_issues_can_be_listed_from_GET_response
     file =  File.read("./authed_issues.json")
-    response = JSON.parse(file.to_json) # <- JSON::ParserError: 757
+    response = JSON.parse(file)
     formatter = FormattedIssue.new
     formatter.format_issues(response)
   end
