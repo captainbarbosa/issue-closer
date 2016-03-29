a759adeb67a2d327811bbdf6f6bdb1e450a896ee
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-28 22:10:11 -0400

    Update authed issues with current list, add partial tests

--- test.rb
+++ test.rb
@@ -1,25 +1,24 @@
 require 'minitest/autorun'
-require 'net/http'
 require 'json'
 require 'pry'
+require './issues'
+require './view'
+require './close_issue'
 
-class Issue
-  def query_list
-  end
-end
+class ApplicationTest < Minitest::Test
+  # def test_classes_exist
+  #   assert Issue
+  #   assert FormattedIssue
+  #   assert CloseIssue
+  # end
 
-class IssueTest < Minitest::Test
-  def test_classes_exist
-    assert Issue
-  end
-
-  def test_user_issues_can_be_listed
-    api = Issue.new
-
-    assert_equal []. api.list
-  end
-
-  def test_change_open_issue_to_closed
+  def test_issues_can_be_listed_from_GET_response
+    file =  File.read("./authed_issues.json")
+    response = JSON.parse(file.to_json) # <- JSON::ParserError: 757
+    formatter = FormattedIssue.new
+    formatter.format_issues(response)
   end
 
+  def test_issue_status_can_be_updated
+  end
 end
