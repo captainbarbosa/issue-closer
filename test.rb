8aadcdbd2af9fafbd63e4f4239e66bf44544f9a7
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2016-05-07 16:46:11 -0400

    Remove unnecessary comments

--- test.rb
+++ test.rb
@@ -6,12 +6,6 @@ require './view'
 require './close_issue'
 
 class ApplicationTest < Minitest::Test
-  # def test_classes_exist
-  #   assert Issue
-  #   assert FormattedIssue
-  #   assert CloseIssue
-  # end
-
   def test_issues_can_be_listed_from_GET_response
     file =  File.read("./authed_issues.json")
     response = JSON.parse(file)
